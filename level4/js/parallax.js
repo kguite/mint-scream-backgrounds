2e115bdc4f2a8c23c33511f967c69a1c2658ec12
KatieMarie <kguite@gmail.com>
2021-08-28 17:09:46 -0700

    level 4 - working parallax background - right to left

--- level4/js/parallax.js
+++ level4/js/parallax.js
@@ -6,23 +6,19 @@ let gameSpeed = 2
 
 
 const backgroundLayer1 = new Image()
-backgroundLayer1.src = 'images/1blue.png';
+backgroundLayer1.src = 'images/1.png';
 const backgroundLayer2 = new Image()
-backgroundLayer2.src = 'images/2blue.png'
+backgroundLayer2.src = 'images/2.png'
 const backgroundLayer3 = new Image()
-backgroundLayer3.src = 'images/3blue.png'
+backgroundLayer3.src = 'images/3.png'
 const backgroundLayer4 = new Image()
-backgroundLayer4.src = 'images/4blue.png'
+backgroundLayer4.src = 'images/4.png'
 const backgroundLayer5 = new Image()
-backgroundLayer5.src = 'images/5blue.png'
+backgroundLayer5.src = 'images/5.png'
 const backgroundLayer6 = new Image()
-backgroundLayer6.src = 'images/6blue.png'
+backgroundLayer6.src = 'images/6.png'
 const backgroundLayer7 = new Image()
-backgroundLayer7.src = 'images/7blue.png'
-const backgroundLayer8 = new Image()
-backgroundLayer8.src = 'images/8blue.png'
-const backgroundLayer9 = new Image()
-backgroundLayer9.src = 'images/9blue.png'
+backgroundLayer7.src = 'images/7.png'
 
 class Layer {
 	constructor(image, speedModifier){
@@ -53,17 +49,16 @@ class Layer {
 	}
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.11);
-const layer2 = new Layer(backgroundLayer2, 0.22);
-const layer3 = new Layer(backgroundLayer3, 0.33);
-const layer4 = new Layer(backgroundLayer4, 0.44);
-const layer5 = new Layer(backgroundLayer5, 0.55);
-const layer6 = new Layer(backgroundLayer6, 0.66);
-const layer7 = new Layer(backgroundLayer7, 0.77);
-const layer8 = new Layer(backgroundLayer8, 0.88);
-const layer9 = new Layer(backgroundLayer9, 0.99); // speed relative to gameSpeed
+const layer1 = new Layer(backgroundLayer1, 0.14);
+const layer2 = new Layer(backgroundLayer2, 0.28);
+const layer3 = new Layer(backgroundLayer3, 0.42);
+const layer4 = new Layer(backgroundLayer4, 0.56);
+const layer5 = new Layer(backgroundLayer5, 0.70);
+const layer6 = new Layer(backgroundLayer6, 0.84);
+const layer7 = new Layer(backgroundLayer7, 1.0);
+ // speed relative to gameSpeed
 
-const gameObjects = [layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8, layer9];
+const gameObjects = [layer1, layer2, layer3, layer4, layer5, layer6, layer7];
 
 function animate(){
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
